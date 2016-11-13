3fa4ee1cec22a8675c14199fcb1cf44dd94d2dc6
Omer Solmazer <os788@nyu.edu>
2016-11-13 17:23:11 -0500

    Renthop crawler finished

--- crawler/data-scripts/RenthopSoup.py
+++ crawler/data-scripts/RenthopSoup.py
@@ -7,100 +7,63 @@ import urllib2
 from bs4 import BeautifulSoup
 
 request_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-request = urllib2.Request('https://www.renthop.com/search/nyc?page=0', headers=request_headers)
+page_limit = 10
 
-response = urllib2.urlopen(request)
-content = response.read()
+# Static strings for finding values in the html tags
 
-content = BeautifulSoup(content, "html.parser")
+link_finder = {"class":"listing-title-link"}
+contact_name_finder = {"class":"font-size-90 color-fg-blue"}
+title_finder = {"style":"font-size: 1.45em; width: 400px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;"}
+neighborhood_finder = {"style":"font-size: 0.95em; margin-top: 4px; color: #666666; width: 400px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;"}
+beds_baths_finder = {"style":"font-weight: bold; color: #444444;"}
+price_finder = {"style":"font-size: 1.45em; color: #005826; text-align: right;"}
+description_finder = {"style":"font-size: 0.90em; line-height: 140%;"}
+photo_finder = {"class":"fotorama"}
 
-href_links = []
+# Create the new listing from the current page and print its json format
+def create_new_listing(page):
+	all_photos = current_page.findAll("div", photo_finder)[0].children
+	photos_count = 0
+	for photo in all_photos:
+		photos_count += 1
 
-for links in content.findAll("a", {"class":"listing-title-link"}):
-    href_links.append(links['href'])
+	contact_name = current_page.find("a",contact_name_finder).text.strip()
+	title = current_page.find("div",title_finder).text.strip()
+	neighborhood = current_page.find("div",neighborhood_finder).text.strip()
 
-for link in href_links:
-    request = urllib2.Request(link, headers=request_headers)
-    current_page = BeautifulSoup(urllib2.urlopen(request).read(), "html.parser")
-    # print(current_page)
-    all_photos = current_page.findAll("div", {"class":"fotorama"})[0].children
-    photos_count = 0
-    for photo in all_photos:
-        photos_count += 1
+	beds_baths = current_page.findAll("span",beds_baths_finder)
+	num_beds = beds_baths[0].text.strip()
+	num_baths = beds_baths[1].text.strip()
 
+	price = current_page.find("div",price_finder).text.strip()
 
-#################################################################################
+	description = current_page.find("div",description_finder).text.strip()
 
-output = {}
-output['num_photos'] = photos_count
+	listing = {}
+	listing['title'] = title
+	listing['neighborhood'] = neighborhood
+	listing['available_date'] = ""
+	listing['num_beds'] = num_beds
+	listing['num_baths'] = num_baths
+	listing['square_ft'] = 0
+	listing['price'] = price
+	listing['description'] = description
+	listing['num_photos'] = photos_count
+	listing['listed_by'] = contact_name
+	listing['link'] = link['href'].strip()
+	
+	time.sleep(1)
 
+	print json.dumps(listing)
 
+# Get page_limit number of pages from renthop and pass them on to get parsed
 
+for i in range(0, page_limit):
+	request = urllib2.Request('https://www.renthop.com/search/nyc?page='+str(i), headers=request_headers)
+	response = urllib2.urlopen(request)
+	content = BeautifulSoup(response.read(), "html.parser")
+	for link in content.findAll("a", link_finder):
+		request = urllib2.Request(link['href'], headers=request_headers)
+		current_page = BeautifulSoup(urllib2.urlopen(request).read(), "html.parser")
+		create_new_listing(current_page)
 
-"""
-title = content.find("span", {"id":"titletextonly"}).text
-neighborhood = content.find("span", {"class": "postingtitletext"}).find("small").text
-available_date = content.find("span", {"class":"property_date"})['data-date']
-price = content.find("span", {"class":"price"}).text;
-num_beds = None;
-num_baths = None;
-square_ft = None;
-listed_by = None;
-
-
-
-# extract attributes from attrgroup
-num_i = 0;
-for child in content.find ("p", {"class":"attrgroup"}).findChildren():
-	# print "\nchild " + str(num_i)
-	# print child
-
-	if(num_i == 1):
-		num_beds = child.text
-	elif(num_i == 2):
-		num_baths = child.text
-	elif(num_i == 4):
-		square_ft = child.text
-	elif(num_i == 7):
-		listed_by = child.text
-	num_i+=1
-
-
-
-
-########################################################################
-
-# create output object
-
-output = {}
-
-output['title'] = title
-output['neighborhood'] = neighborhood
-output['available_date'] = available_date
-output['num_beds'] = num_beds
-output['num_baths'] = num_baths
-output['square_ft'] = square_ft
-output['listed_by'] = listed_by
-output['price'] = price
-
-
-print json.dumps(output);
-"""
-
-"""
-"title": "Example Listing 1",
-		"lat": 111.111,
-		"lng": 111.111,
-		"neighborhood": "LES",
-		"date": "06-24-2016",
-		"description": "an example apartment listing (Lower East Side)",
-		"contact_email": "contact@example.com",
-		"contact_phone": "111-111-1111",
-		"price": 4500,
-		"bedrooms": 3,
-		"bathrooms": 2,
-		"has_livingroom": true,
-		"has_kitchen": true,
-		"allows_pets": false,
-		"score": 9.3
-"""
