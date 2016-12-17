59cc2ac2893f6f675f938739bdf9c0deb919daea
Omer Solmazer <os788@nyu.edu>
2016-12-17 17:45:36 -0500

    Renthop outputs reformatted. Craigslist code is restructured

--- crawler/data-scripts/RenthopSoup.py
+++ crawler/data-scripts/RenthopSoup.py
@@ -1,98 +1,105 @@
-import sys
-import json
-import csv
 import time
-import os.path
 import urllib2
 import re
 from bs4 import BeautifulSoup
-import pymongo
+import traceback
 from pymongo import MongoClient
 from selenium import webdriver
 
-request_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-page_limit = 10
+request_headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/39.0.2171.95 Safari/537.36'}
+page_limit = 20
 driver = webdriver.Chrome()
 
 # Static strings for finding values in the html tags
 
-link_finder = {"class":"listing-title-link"}
-contact_name_finder = {"class":"font-size-90 color-fg-blue"}
-title_finder = {"style":"font-size: 1.45em; width: 400px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;"}
-neighborhood_finder = {"style":"font-size: 0.95em; margin-top: 4px; color: #666666; width: 400px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;"}
-beds_baths_finder = {"style":"font-weight: bold; color: #444444;"}
-price_finder = {"style":"font-size: 1.45em; color: #005826; text-align: right;"}
-description_finder = {"style":"font-size: 0.90em; line-height: 140%;"}
-photo_finder = {"class":"fotorama"}
-available_date_finder = {"style":"font-size: 0.95em; color: #666666; padding-left: 10px; border-left: 1px solid #eeeeee;"}
-lat_long_finder = {"title":"Click to see this area on Google Maps"}
+link_finder = {"class": "listing-title-link"}
+contact_name_finder = {"class": "font-size-90 color-fg-blue"}
+title_finder = {
+    "style": "font-size: 1.45em; width: 400px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;"}
+neighborhood_finder = {
+    "style": "font-size: 0.95em; margin-top: 4px; color: #666666; width: 400px; white-space: nowrap; text-overflow: "
+             "ellipsis; overflow: hidden;"}
+beds_baths_finder = {"style": "font-weight: bold; color: #444444;"}
+price_finder = {"style": "font-size: 1.45em; color: #005826; text-align: right;"}
+description_finder = {"style": "font-size: 0.90em; line-height: 140%;"}
+photo_finder = {"class": "fotorama"}
+available_date_finder = {
+    "style": "font-size: 0.95em; color: #666666; padding-left: 10px; border-left: 1px solid #eeeeee;"}
+lat_long_finder = {"title": "Click to see this area on Google Maps"}
+
 
 def get_db():
-	connection = MongoClient('ds023654.mlab.com', 23654)
-	db = connection['apartmentdb']
-	db.authenticate('admin', 'craigslistsucks')
+    connection = MongoClient('ds023654.mlab.com', 23654)
+    db = connection['apartmentdb']
+    db.authenticate('admin', 'craigslistsucks')
 
-	return db
+    return db
 
-db = get_db()
 
-# Create the new listing from the current page and print its json format
-def create_new_listing(current_page):
-	all_photos = current_page.findAll("div", photo_finder)[0].children
-	photos_count = 0
-	for photo in all_photos:
-		photos_count += 1
+# Create the new listing from the current page and return its
+def create_new_listing(current_page, link):
+    try:
+        all_photos = current_page.findAll("div", photo_finder)[0].children
+        photos_count = 0
+        for _ in all_photos:
+            photos_count += 1
 
-	contact_name = current_page.find("a",contact_name_finder).text.strip()
-	title = current_page.find("div",title_finder).text.strip()
-	neighborhood = current_page.find("div",neighborhood_finder).text.strip()
+        contact_name = current_page.find("a", contact_name_finder).text.encode('utf-8').strip()
+        title = current_page.find("div", title_finder).text.strip()
+        neighborhood = current_page.find("div", neighborhood_finder).text.encode('utf-8').strip()
 
-	beds_baths = current_page.findAll("span",beds_baths_finder)
-	num_beds = beds_baths[0].text.strip()
-	num_baths = beds_baths[1].text.strip()
+        beds_baths = current_page.findAll("span", beds_baths_finder)
+        # STUDIO = 0 num_beds
+        try:
+            num_beds = int(beds_baths[0].text.strip())
+        except ValueError:
+            num_beds = 0
+        num_baths = int(beds_baths[1].text.strip())
 
-	price = current_page.find("div",price_finder).text.strip()
-	description = current_page.find("div",description_finder).text.strip()
-	available_date = current_page.find("td", available_date_finder).text.strip()
-	lat_long = current_page.find("a",lat_long_finder)['href']
-	
-	parsed = re.search('(.*)ll=(.*)z=(.*)',lat_long)
-	latitude = str(parsed.group(2)[:len(parsed.group(2))-2].split(',')[0])
-	longitude = str(parsed.group(2)[:len(parsed.group(2))-2].split(',')[1])
+        price = float(current_page.find("div", price_finder).text.replace("$", "").replace(",", ".").strip())
+        description = current_page.find("div", description_finder).encode('utf-8').strip()
+        # Available date can be either Immediate or MM-DD-YYYY
+        available_date = current_page.find("td", available_date_finder).text.strip()
+        lat_long = current_page.find("a", lat_long_finder)['href']
 
-	# craigslist has square foot info available but renthop does
-	listing = {}
-	listing['title'] = str(title)
-	listing['neighborhood'] = str(neighborhood)
-	listing['available_date'] = str(available_date)
-	listing['num_beds'] = str(num_beds)
-	listing['num_baths'] = str(num_baths)
-	listing['square_ft'] = str(0,0)
-	listing['price'] = str(price)
-	listing['description'] = str(description)
-	listing['num_photos'] = str(photos_count)
-	listing['listed_by'] = str(contact_name)
-	listing['link'] = str(link['href'].strip())
-	listing['latitude'] = str(latitude)
-	listing['longitude'] = str(longitude)
-	
+        parsed = re.search('(.*)ll=(.*)z=(.*)', lat_long)
+        latitude = float(parsed.group(2)[:len(parsed.group(2)) - 2].split(',')[0])
+        longitude = float(parsed.group(2)[:len(parsed.group(2)) - 2].split(',')[1])
 
-	print json.dumps(listing)
-	# db.listings.insert(listing)
+        # craigslist has square foot info available but renthop does not
+        listing = {'title': title, 'neighborhood': neighborhood, 'available_date': available_date,
+                   'num_beds': num_beds, 'num_baths': num_baths, 'square_ft': 0, 'price': price,
+                   'description': description, 'num_photos': photos_count, 'listed_by': contact_name,
+                   'link': link['href'].strip().encode('utf-8').strip(), 'latitude': latitude, 'longitude': longitude}
+        # listing['link'] = str(link['href'].strip())
+        # print json.dumps(listing)
+        return listing
+    except ValueError:
+        print("Value error ", traceback.print_exc())
+    except:
+        print("Unexpected error ", traceback.print_exc())
+
 
 # Get page_limit number of pages from renthop and pass them on to get parsed
 
-for i in range(0, page_limit):
-	request = urllib2.Request('https://www.renthop.com/search/nyc?page='+str(i), headers=request_headers)
-	response = urllib2.urlopen(request)
-	content = BeautifulSoup(response.read(), "html.parser")
-	for link in content.findAll("a", link_finder):
-		# request = urllib2.Request(link['href'], headers=request_headers)
-		driver.get(link['href'])
-		time.sleep(5)
-		soup = BeautifulSoup(driver.page_source,"html.parser")
-		
-		# current_page = BeautifulSoup(urllib2.urlopen(request).read(), "html.parser")
-		create_new_listing(soup)
+def main():
+    db = get_db()
+    for i in range(0, page_limit):
+        request = urllib2.Request('https://www.renthop.com/search/nyc?page=' + str(i), headers=request_headers)
+        response = urllib2.urlopen(request)
+        content = BeautifulSoup(response.read(), "html.parser")
+        for link in content.findAll("a", link_finder):
+            # request = urllib2.Request(link['href'], headers=request_headers)
+            driver.get(link['href'])
+            time.sleep(5)
+            soup = BeautifulSoup(driver.page_source, "html.parser")
+            listing = create_new_listing(soup, link)
+            db.listings.insert(listing)
 
-driver.close()
+    driver.close()
+
+
+if __name__ == "__main__":
+    main()
--- crawler/data-scripts/craigslist.py
+++ crawler/data-scripts/craigslist.py
@@ -1,177 +1,125 @@
-import sys
 import json
-import csv
 import time
-import os.path
 import urllib2
 import re
 from bs4 import BeautifulSoup
-import pymongo
 from pymongo import MongoClient
 from selenium import webdriver
 
 
-# ds023654.mlab.com:23654/apartmentdb
 def get_db():
-	connection = MongoClient('ds023654.mlab.com', 23654)
-	db = connection['apartmentdb']
-	db.authenticate('admin', 'craigslistsucks')
+    connection = MongoClient('ds023654.mlab.com', 23654)
+    db = connection['apartmentdb']
+    db.authenticate('admin', 'craigslistsucks')
+    return db
 
-	return db
 
-db = get_db()
 driver = webdriver.Chrome()
 
-# >>> from pymongo import MongoClient
-# >>> client = pymongo.MongoClient('example.com')
-# >>> client.the_database.authenticate('user', 'password')
-# True
-# >>>
-# >>> uri = "mongodb://user:password@example.com/the_database"
-# >>> client = pymongo.MongoClient(uri)
-# >>>
+request_headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/39.0.2171.95 Safari/537.36'}
 
 
-request_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+def main():
+    db = get_db()
+    for min_rent in range(1000, 2000, 500):
+        for page in range(0, 2500, 100):
+            url = "https://newyork.craigslist.org/search/aap?s=" + str(page) + "&max_price=" + str(
+                min_rent + 500) + "&min_price=" + str(min_rent)
+            request = urllib2.Request(url, headers=request_headers)
+            response = urllib2.urlopen(request)
+            content = BeautifulSoup(response.read(), "html.parser")
+            output = create_new_listings(content)
+            db.listings.insert(output)
 
-for min_rent in range(1000,2000,500):
-	for page in range(0,2500,100):	
-		url="https://newyork.craigslist.org/search/aap?s="+str(page)+"&max_price="+str(min_rent+500)+"&min_price="+str(min_rent)
-		request = urllib2.Request(url, headers=request_headers)
-
-		response = urllib2.urlopen(request)
-		content = response.read()
-
-		content = BeautifulSoup(content, "html.parser")
-
-		# href_links = []
-
-		for links in content.findAll("a", {"class":"hdrlnk"}):
-			# href_links.append("https://newyork.craigslist.org"+links['href'])
-			request_url = "https://newyork.craigslist.org"+links['href']
-			# print "request URL: " + request_url
+    driver.close()
+    db.logout()
 
 
-			# download page as HTML file
-			# print("downloading... " + request_url + "\n")
+def create_new_listings(content):
+    for links in content.findAll("a", {"class": "hdrlnk"}):
+        request_url = "https://newyork.craigslist.org" + links['href']
 
-			driver.get(request_url)
-			time.sleep(5)
-			content = BeautifulSoup(driver.page_source,"html.parser")
-		
-# 		create_new_listing(soup)
+        driver.get(request_url)
+        time.sleep(5)
+        content = BeautifulSoup(driver.page_source, "html.parser")
 
-			# request_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-			# request = urllib2.Request(request_url, headers=request_headers)
+        try:
+            title = content.find("span", {"id": "titletextonly"}).text
+        except AttributeError:
+            title = "None"
 
-			# response = urllib2.urlopen(request)
-			# content = response.read()
+        try:
+            neighborhood = content.find("span", {"class": "postingtitletext"}).find("small").text
+        except AttributeError:
+            neighborhood = "None"
 
+        try:
+            available_date = content.find("span", {"class": "property_date"})['data-date']
+        except AttributeError:
+            available_date = "None"
 
-			# extract data from HTML document 
-			# content = BeautifulSoup(content, "html.parser")
+        try:
+            price = content.find("span", {"class": "price"}).text
+        except AttributeError:
+            price = "None"
 
-			# declare data fields
+        num_beds = None
+        num_baths = None
+        square_ft = None
+        listed_by = None
 
-			try:
-				title = content.find("span", {"id":"titletextonly"}).text
-			except AttributeError:
-				title = "None"
+        # extract attributes from attrgroup
+        num_i = 0
+        for child in content.find("p", {"class": "attrgroup"}).findChildren():
+            # print "\nchild " + str(num_i)
+            # print child
 
-			try:
-				neighborhood = content.find("span", {"class": "postingtitletext"}).find("small").text
-			except AttributeError:
-				neighborhood = "None"
+            if num_i == 1:
+                num_beds = child.text
+            elif num_i == 2:
+                num_baths = child.text
+            elif num_i == 4:
+                square_ft = child.text
+            elif num_i == 7:
+                listed_by = child.text
+            num_i += 1
 
-			try:
-				available_date = content.find("span", {"class":"property_date"})['data-date']
-			except AttributeError:
-				available_date = "None"
+        latitude = 0
+        longitude = 0
+        lat_long = content.find("a", {"target": "_blank"})
+        if lat_long is not None:
+            print(lat_long['href'])
+            parsed = re.search('(.*)@(.*)z(.*)', lat_long['href'])
+            if parsed is not None:
+                latitude = str(parsed.group(2)[:len(parsed.group(2)) - 2].split(',')[0])
+                longitude = str(parsed.group(2)[:len(parsed.group(2)) - 2].split(',')[1])
+            else:
+                inner_driver = webdriver.Chrome()
+                inner_driver.get(lat_long['href'])
+                time.sleep(5)
+                new_lat_long = inner_driver.current_url.split('@')[1].split(',')
+                latitude = str(new_lat_long[0])
+                longitude = str(new_lat_long[1])
+                inner_driver.close()
 
-			try:
-				price = content.find("span", {"class":"price"}).text;
-			except AttributeError:
-				price = "None"		
+        description = content.find("section", {"id": "postingbody"}).text.strip()
+        photo_wrapper = content.find("span", {"class": "slider-info"})
+        num_photos = 0
+        if photo_wrapper is not None:
+            num_photos = photo_wrapper.text.strip().split(" ")[3]
 
+        output = {'title': str(title), 'neighborhood': str(neighborhood), 'available_date': str(available_date),
+                  'num_beds': str(num_beds), 'num_baths': str(num_baths), 'square_ft': str(square_ft),
+                  'listed_by': str(listed_by), 'price': str(price), 'latitude': str(latitude),
+                  'longitude': str(longitude), 'link': str(driver.current_url), 'description': str(description),
+                  'num_photos': str(num_photos)}
 
-			num_beds = None;
-			num_baths = None;
-			square_ft = None;
-			listed_by = None;
-
-
-
-			# extract attributes from attrgroup
-			num_i = 0;
-			for child in content.find ("p", {"class":"attrgroup"}).findChildren():
-				# print "\nchild " + str(num_i)
-				# print child
-				
-				if(num_i == 1):
-					num_beds = child.text
-				elif(num_i == 2):
-					num_baths = child.text
-				elif(num_i == 4):
-					square_ft = child.text
-				elif(num_i == 7):
-					listed_by = child.text
-				num_i+=1
-
-			latitude = 0
-			longitude = 0
-			lat_long = content.find("a",{"target":"_blank"})
-			if lat_long != None :
-				print(lat_long['href'])
-				parsed = re.search('(.*)@(.*)z(.*)',lat_long['href'])
-				if parsed != None:
-					latitude = str(parsed.group(2)[:len(parsed.group(2))-2].split(',')[0])
-					longitude = str(parsed.group(2)[:len(parsed.group(2))-2].split(',')[1])
-				else:
-					inner_driver = webdriver.Chrome()
-					inner_driver.get(lat_long['href'])
-					time.sleep(5)
-					new_lat_long = inner_driver.current_url.split('@')[1].split(',')
-					latitude = str(new_lat_long[0])
-					longitude = str(new_lat_long[1])
-					inner_driver.close()
-			
-			description = content.find("section",{"id":"postingbody"}).text.strip()
+        print json.dumps(output)
+        time.sleep(1)
+        return output
 
 
-			########################################################################
-
-			# create output object
-
-
-			photo_wrapper = content.find("span",{"class":"slider-info"})
-			num_photos = 0
-			if photo_wrapper != None:
-				num_photos = photo_wrapper.text.strip().split(" ")[3]
-			
-			output = {}
-
-			output['title'] = str(title)
-			output['neighborhood'] = str(neighborhood)
-			output['available_date'] = str(available_date)
-			output['num_beds'] = str(num_beds)
-			output['num_baths'] = str(num_baths)
-			output['square_ft'] = str(square_ft)
-			output['listed_by'] = str(listed_by)
-			output['price'] = str(price)
-			output['latitude'] = str(latitude)
-			output['longitude'] = str(longitude)
-			output['link'] = str(driver.current_url)
-			output['description'] = str(description)
-			output['num_photos'] = str(num_photos)
-
-
-
-			print json.dumps(output);
-			# db.listings.insert(output);
-
-			time.sleep(1)
-
-					
-
-driver.close()
-		
+if __name__ == "__main__":
+    main()
