de7dd8a80dc3d93c6264957baddff4052bdd5f5c
Evan Feng <evanfeng55@gmail.com>
2016-12-18 21:07:58 -0500

    implemented color scale

--- searcher/public/main.js
+++ searcher/public/main.js
@@ -25,6 +25,16 @@ document.addEventListener('DOMContentLoaded', function(){
 	    zoom: 11
 	});
 
+	// create color scale function
+	var colorscale = d3.scale.linear()
+		.domain([-1000, 1000])
+		.range(["red", "blue"]);
+
+	//get neighborhood stats
+	var neighborhood_stats;
+	$.get("/neighborhoods", function(response){
+		neighborhood_stats = response;
+	}.bind(this));
 
 
 	//bind an event listener to all search fields
@@ -36,26 +46,47 @@ document.addEventListener('DOMContentLoaded', function(){
 			var numbathrooms_input = $(".numbathrooms").val();
 			var price_high = $(".rightLabel").text();
 			var price_low = $(".leftLabel").text();
-
-			// alert("new search:\n" + neighborhood_input + "\n" + numbedrooms_input + "\n" + numbathrooms_input + "\n" + price_low + "\n" + price_high);
 			
 			var request_url = "/listings?hood=" + neighborhood_input + "&numbedrooms=" + numbedrooms_input + "&numbathrooms=" + numbathrooms_input + "&pricelow=" + price_low + "&pricehigh=" + price_high;
-			// alert(request_url);
 
+
+			console.log(neighborhood_stats);
 			$.get(request_url, function(response){
-				console.log(response);
+
+				// clear markers
+				$(".mapboxgl-marker").remove();
 
 				for(var i=0; i<response.length; i++){
 					var lat_val = response[i]['latitude'];
 					var lng_val = response[i]['longitude']
 
-					var marker = new mapboxgl.Marker().setLngLat([lng_val, lat_val]).addTo(map);
-					console.log(marker);
+					//get price difference from neighborhood avg
+					var price_diff = 0.0;
+					for(var j=0; j<neighborhood_stats.length; j++){
+						if( neighborhood_stats[j]['name'] == (response[i]['neighborhood']+"_"+response[i]['num_beds']) ){
+							price_diff = response[i]['price'] - neighborhood_stats[j]['avg_price'];
+							break;
+						}
+					}
+
+					var listing_text = "<b>" + response[i]['title'] + "</b><br/>";
+					listing_text += "<b>Price:  </b>$" + response[i]['price'] + "\t<em>(" + price_diff + ")</em><br/>";
+					listing_text += response[i]['num_beds'] + "Bedrooms\t" + response[i]['num_baths'] + "Bathrooms<br/>";
+					listing_text += "<em><a target='_blank' href='" + response[i]['link'] + "'>source: " + response[i]['link'].split(".")[1] + "</a></em>";
+
+					var popup = new mapboxgl.Popup({offset:[0, -30]}).setHTML(listing_text);
+
+					var el = document.createElement('div');
+					el.className = 'mapboxgl-marker';
+					el.style.backgroundColor = colorscale(price_diff);
+
+					var marker = new mapboxgl.Marker(el).setLngLat([lng_val, lat_val]).setPopup(popup).addTo(map);
 				}
 			}.bind(this));
 		}
 	}.bind(this));
 
+	
 
 
 
